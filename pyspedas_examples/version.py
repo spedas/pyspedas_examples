945ee7c60b83c0a940c33e7ab1ac78558dd976ac
Nick H <34072991+nickssl@users.noreply.github.com>
2023-12-09 14:54:05 -0800

    Update version.py

--- pyspedas_examples/version.py
+++ pyspedas_examples/version.py
@@ -9,14 +9,35 @@ Returns
 
 def version():
     """Versions of main packages."""
+
     import pkg_resources
+
     try:
         ver = pkg_resources.get_distribution("pyspedas_examples").version
+        print("pyspedas_examples version: " + ver)
     except pkg_resources.DistributionNotFound:
         print("pyspedas_examples is not installed as a package, version info not available")
-        return False
-    print("pyspedas_examples version: " + ver)
-    verp = pkg_resources.get_distribution("pyspedas").version
-    print("pyspedas version: " + verp)
-    verp = pkg_resources.get_distribution("pytplot").version
-    print("pytplot version: " + verp)
+
+    try:
+        verp = pkg_resources.get_distribution("pyspedas").version
+        print("pyspedas version: " + verp)
+    except pkg_resources.DistributionNotFound:
+        print("pyspedas is not installed as a package, version info not available")
+
+    try:
+        vern = pkg_resources.get_distribution("pytplot-mpl-temp").version
+        print("pytplot-mpl-temp version: " + vern)
+    except pkg_resources.DistributionNotFound:
+        print("pytplot-mpl-temp is not installed as a package, version info not available")
+
+    try:
+        vero = pkg_resources.get_distribution("pytplot").version
+        print("pytplot (original) version: " + vero)
+    except pkg_resources.DistributionNotFound:
+        print(
+            "pytplot (original) is not installed as a package, version info not available")
+
+
+# Run the version function directly
+if __name__ == '__main__':
+    version()
