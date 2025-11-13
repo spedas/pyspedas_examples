49b6066f7e13c8d410ef29fd70344c93697ba455
jameswilburlewis <jwl@ssl.berkeley.edu>
2025-11-12 17:08:32 -0800

    Use importlib instead of deprecated pkt_resources; remove version checks for pytplot and pytplot-mpl-temp

--- pyspedas_examples/version.py
+++ pyspedas_examples/version.py
@@ -5,43 +5,32 @@ Returns
 -------
     The version number for the current installation.
 """
+# We don't want to shadow the importlib version() with pyspedas(version)
+from importlib.metadata import version as imp_version
+from importlib.metadata import PackageNotFoundError
 
 
 def version():
     """Versions of main packages."""
 
-    import pkg_resources
 
     try:
-        ver = pkg_resources.get_distribution("pyspedas_examples").version
-        print("pyspedas_examples version: " + ver)
-    except pkg_resources.DistributionNotFound:
-        print("pyspedas_examples is not installed as a package, version info not available")
+        ver = imp_version("pyspedas_examples")
+    except PackageNotFoundError:
+        ver = 'bleeding edge'
+    print("pyspedas_examples version: " + ver)
 
     try:
-        verp = pkg_resources.get_distribution("pyspedas").version
-        print("pyspedas version: " + verp)
-    except pkg_resources.DistributionNotFound:
-        print("pyspedas is not installed as a package, version info not available")
+        ver = imp_version("pyspedas")
+    except PackageNotFoundError:
+        ver = 'bleeding edge'
+    print("pyspedas version: " + ver)
 
     try:
-        vern = pkg_resources.get_distribution("pytplot-mpl-temp").version
-        print("pytplot-mpl-temp version: " + vern)
-    except pkg_resources.DistributionNotFound:
-        print("pytplot-mpl-temp is not installed as a package, version info not available")
-
-    try:
-        vero = pkg_resources.get_distribution("pytplot").version
-        print("pytplot (original) version: " + vero)
-    except pkg_resources.DistributionNotFound:
-        print(
-            "pytplot (original) is not installed as a package, version info not available")
-
-    try:
-        vero = pkg_resources.get_distribution("cdasws").version
-        print("cdasws version: " + vero)
-    except pkg_resources.DistributionNotFound:
-        print("cdasws is not installed as a package, version info not available")
+        ver = imp_version("cdasws")
+    except PackageNotFoundError:
+        ver = 'bleeding edge'
+    print("cdasws version: " + ver)
 
 
 # Run the version function directly
